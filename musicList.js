48aef626f2361475304b3ecd70ab60df03ea5464
piyushdhale777 <piyushdhale777@gmail.com>
2023-12-29 13:19:36 +0530

    Music palyer

---
+++ musicList.js
@@ -0,0 +1,75 @@
+
+let allMusic = [
+  {
+    name: " Aaoge Tum Kabhi ",
+    artist: "The Local Train",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Alag Aasmaan",
+    artist: "Anuv Jain",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Choo Lo ",
+    artist: "The Local Train",
+    img: "music-1",
+    src: "music-3"
+  },
+  {
+    name: "Din Dhalay",
+    artist: "Bayaan",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Give Me Some Sunshine ",
+    artist: "Suraj Jagan, Sharman Joshi",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Jogi",
+    artist: "Swastik The Band",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Kya Karein",
+    artist: "Aditya Rikhari",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Milte Raho",
+    artist: "Vaibhav Bundhoo",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Mohbhang",
+    artist: "Parul Mishra",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Samjho Na",
+    artist: "Aditya Rikhari",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Tu Aisa Kaise Hai",
+    artist:"Osho Jain",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Yaarr Ni Milyaa",
+    artist: "Harrdy Sandhu, B Praak, Jaani",
+    img: "music-12",
+    src: "music-12"
+  },
+];
